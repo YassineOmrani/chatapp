0fddfbe71e0407c285ebdd2d97ebf3e438193f32
YassineOmrani <yassineeomrani@gmail.com>
2023-09-28 00:10:03 +0200

    add message entity

--- backend/src/main/java/com/backend/chat/data/Message.java
+++ backend/src/main/java/com/backend/chat/data/Message.java
@@ -1,4 +1,33 @@
 package com.backend.chat.data;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "messages")
 public class Message {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Column(name = "message")
+    @NonNull
+    String message;
+    @Column(name = "sender")
+    @NonNull
+    String sender;
+
 }
